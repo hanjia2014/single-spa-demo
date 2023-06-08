e32d4419564c60193cd8c6fb38cb0563d778d681
Han <hanj@datacom.co.nz>
2023-06-09 09:45:27 +1200

    refactor

--- single-spa-demo-child/webpack.config.js
+++ single-spa-demo-child/webpack.config.js
@@ -4,7 +4,7 @@ const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "han-demo",
-    projectName: "single-spa-demo-nav",
+    projectName: "single-spa-demo-child",
     webpackConfigEnv,
     argv,
   });
@@ -12,7 +12,7 @@ module.exports = (webpackConfigEnv, argv) => {
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
     devServer: {
-      port: 8089
+      port: 8090
     }
   });
 };
