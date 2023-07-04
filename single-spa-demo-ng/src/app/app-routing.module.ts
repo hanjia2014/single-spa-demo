673cceb636ead8b0e3540bfdc2d6ea1eb956b62c
Han <hanj@datacom.co.nz>
2023-07-04 13:29:41 +1200

    add empty routing

--- single-spa-demo-ng/src/app/app-routing.module.ts
+++ single-spa-demo-ng/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 const routes: Routes = [
   {
     path: 'ng',
     component: HomeComponent
+  },
+  {
+    path: '**',
+    component: EmptyRouteComponent
   }
 ];
 
