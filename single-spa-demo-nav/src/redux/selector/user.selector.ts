e4f08a4ee1e822fef21592b9741dd0cc13b00531
Han <hanj@datacom.co.nz>
2023-08-03 12:39:48 +1200

    add more selector

--- single-spa-demo-nav/src/redux/selector/user.selector.ts
+++ single-spa-demo-nav/src/redux/selector/user.selector.ts
@@ -1,3 +1,9 @@
+import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
 export const selectUsers = (state: RootState) => state.userState.users;
+
+export const selectUserById = id => createSelector(
+    selectUsers,
+    users => (users || []).filter((u: any) => u.id === id)
+)
