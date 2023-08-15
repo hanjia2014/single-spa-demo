6f5a037143852431f94739b0c42339489dddbf46
Han <hanj@datacom.co.nz>
2023-08-15 13:48:58 +1200

    add webpack rules

--- single-spa-demo-child/webpack.config.js
+++ single-spa-demo-child/webpack.config.js
@@ -13,6 +13,27 @@ module.exports = (webpackConfigEnv, argv) => {
     // modify the webpack config however you'd like to by adding to this object
     devServer: {
       port: 8090
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/, // .js and .jsx files
+          exclude: /node_modules/, // excluding the node_modules folder
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.(sa|sc|c)ss$/, // styles files
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.([cm]?ts|tsx)$/, loader: "ts-loader"
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
     }
   });
 };
